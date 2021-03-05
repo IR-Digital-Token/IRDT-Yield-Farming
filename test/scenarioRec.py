35326d0f6f555588e8e28ff76739c43ffeb3fe60
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2021-03-05 16:59:51 +0330

    Some Prettier Thing

--- test/scenarioRec.py
+++ test/scenarioRec.py
@@ -1,28 +1,34 @@
 from scenarioGen import Plan, User, forwardTime
+import pprint
 
-rewardAmount = 2000
-startTime = 50
-duration = 100
-times = [
-    {'time': startTime, 'is_stake': True, 'stake_amount': 1000},
-    {'time': (startTime + duration), 'is_stake': False, 'unstake_num': 1, 'unstake_amount': 0}
-]
-referralEnable = False
-referralPercent = 0
+norm_start_time = 50
+norm_duration = 100
+test_1 = {
+    'reward_amount': 2000,
+    'start_time': norm_start_time,
+    'duration': norm_duration,
+    'times': [
+        {'time': norm_start_time, 'is_stake': True, 'stake_amount': 1000},
+        {'time': (norm_start_time + norm_duration), 'is_stake': False, 'unstake_num': 1, 'unstake_amount': 0}
+    ],
+    'referral_enable': False,
+    'referral_percent': 0,
+}
 
 
-def fill_stake_amounts(reward_amount, start_time, stake_duration, stake_times, referral_enable, referral_percent):
-    plan = Plan(reward_amount, start_time, stake_duration, referral_enable, referral_percent)
+def fill_stake_amounts(scenario):
+    plan = Plan(scenario['reward_amount'], scenario['start_time'], scenario['duration'],
+                scenario['referral_enable'], scenario['referral_percent'])
     users = []
     before_time = 0
-    for time in stake_times:
+    for time in scenario['times']:
         forwardTime(time['time'] - before_time)
         if time['is_stake']:
             users.append(User(plan, time['stake_amount']))
         else:
             time['unstake_amount'] = users[time['unstake_num'] - 1].unStake(plan)
         before_time = time['time']
-    print(stake_times)
+    pprint.PrettyPrinter().pprint(scenario)
 
 
-fill_stake_amounts(rewardAmount, startTime, duration, times, referralEnable, referralPercent)
+fill_stake_amounts(test_1)
